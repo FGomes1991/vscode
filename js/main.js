e32bc36df26e3cd6fead46d059ffb23f6dc29b21
Felip Gomes <f62843@bancoexterior.com>
2025-03-18 23:34:29 -0400

    cambio7

--- js/main.js
+++ js/main.js
@@ -6,5 +6,5 @@ function sumaArray(arr) {
     return suma;
 }
 
-let numeros = [1, 2, 3, 4, 5];
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(`La suma del array es: ${sumaArray(numeros)}`);
